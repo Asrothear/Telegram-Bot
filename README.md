885265a01ec13828226ba73c91998724c9a81c0e
Asrothear <lord@asrothear.de>
2020-09-26 01:46:12 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-Telegram-Bot
+# Telegram-Bot Example
+
+[![API](https://img.shields.io/badge/TelegramAPI-4.8-green)](https://core.telegram.org/bots/api) [![winston](https://img.shields.io/badge/winston-3.3.3%2B-green)](https://www.npmjs.com/package/winston) [![node](https://img.shields.io/badge/node->%3D12-green)](https://nodejs.org/en/) [![Telegraf](https://img.shields.io/badge/telegraf.js-3.38-green)](https://www.npmjs.com/package/telegraf) 
+
+This is a simple Telegram-Bot based on JS and the Telegraf package.
+
+  - Fast Setup
+  - Custom Commands
+  - Basic mysql example Inclued
+
+
+### Tech
+This Bot uses open source software:
+* [node.js](https://nodejs.org/) - Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+* [Telegraf](https://www.npmjs.com/package/telegraf) - Modern Telegram BotFramework for Node.js
+* [mysql](https://www.npmjs.com/package/mysql) - awesome mysql driver
+* [winston](https://www.npmjs.com/package/winston) - awsome logger library 
+
+
+### Installation
+Requirements:
+* [Node.js](https://nodejs.org/) v12+
+* [mysql](https://www.npmjs.com/package/mysql) v2+
+* [Telegraf](https://www.npmjs.com/package/telegraf) v3+
+* [winston](https://www.npmjs.com/package/winston) - v3+
+
+To install node v12 run the following commands:
+```sh
+$ sudo apt update
+$ sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+$ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+$ sudo apt -y install nodejs
+```
+Install the dependencies fast with the npm installer:
+```sh
+$ cd telegram-Bot
+$ npm install
+```
+
+add your Bot-Token and if needed your Databe login in the `config.mjs`
+
+to run this Bot simply use
+```sh
+$ node bot.mjs
+```
+
+## Recommendation
+It will be usefull to run this Bot via a [screen](https://wiki.ubuntuusers.de/Screen/) session.
+
+### Development
+Want to contribute? Great!
+Just Create a Pull requests
+
+
+### Todos
+ - Write MORE Examples
+ - Add general mysql functionality
+
+License
+----
+
+GNU GENERAL PUBLIC LICENSE v3+
---
+++ bot.mjs
@@ -0,0 +1,177 @@
+import mysql from 'mysql';
+import winston from 'winston';
+import Telegraf from 'telegraf';
+import Config from './config.mjs';
+var bol = false
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.json(),
+    maxsize: 500,
+    transports: [new winston.transports.File({ filename: 'debug.log'})],
+});
+
+/*
+logger.error('Error message');
+logger.info('Information message');
+logger.warn('Warning message');
+logger.debug('Some message');
+*/
+
+const bot = new Telegraf(Config.token)
+
+function init_db(){
+    let DB = mysql.createConnection({
+        host: Config.db.host,
+        user: Config.db.user,
+        password: Config.db.passwort,
+        database: Config.db.database
+    });
+
+    DB.connect(function(err) {
+        if (err){
+            log(err);
+            logger.error(err);
+            return false
+        }
+    });
+    return DB
+}
+
+bot.catch((err, ctx) => {
+    log(`Error on ${ctx.updateType}: ${err}`)
+    logger.error(`Error on ${ctx.updateType}: ${err}`);
+})
+
+function db_query(ctx,sql){
+    let DB = init_db();
+    let bol = DB.query(sql, function (err, result) {
+        if (err){
+            if(Config.debug){
+                ctx.reply("ERROR: "+err.sqlMessage);
+            };
+            log(err.sqlMessage)
+            logger.error(err.sqlMessage);
+            return
+        };
+        log("Database query executed");
+        logger.info("Database query executed")
+        if(Config.debug){
+            ctx.reply("Database query executed")
+        };
+        return
+    });
+    DB.end()
+    return bol
+}
+
+function log(txt){
+    console.log(`Xerxes: ${txt}`)
+}
+
+function get_datetime(x)
+{
+    let date = new Array("","");
+    let now = Date();
+    date[0] = now.substr(11,13)
+    date[1] = now.substr(0,15)
+    return date[x]
+}
+
+
+bot.help((ctx) => ctx.reply('404 hilfe nicht gefunden, schon mal unterm Teppich geschaut ??'))
+bot.command('mesast_qrv', mesastqrv)
+bot.command('mesast_qrt', mesastqrt)
+bot.command('el_qrv', elqrv)
+bot.command('el_qrt', elqrt)
+bot.command('einsatzmeldung', einsatzmeldung)
+bot.command('meldungsabfrage', meldungsabfrage)
+bot.command('stabsabfrage', stabsabfrage)
+
+function mesastqrv(ctx, next){
+    let user = ctx.from.username
+    let table = 'bereitschaftslog'
+    let sql =`INSERT INTO ${table} (time, user, rang, status) VALUES("${get_datetime(0)}-${get_datetime(1)}", "${user}", "MESAST", "aktiv");`
+    let result = db_query(ctx,sql)
+    if(result){
+        ctx.reply(`${user} meldet sich als MESAST QRV`)
+    };
+    logger.info(`${user} meldet sich als MESAST QRV`);
+}
+function mesastqrt(ctx, next){
+    let user = ctx.from.username
+    let table = 'bereitschaftslog'
+    let sql =`INSERT INTO ${table} (time, user, rang, status) VALUES("${get_date()}", "${user}", "MESAST", "inaktiv");`
+    let result = db_query(ctx,sql)
+    if(result){
+        ctx.reply(`${user} meldet sich als MESAST ab`)
+    };
+    logger.info(`${user} meldet sich als MESAST ab`);
+}
+function elqrv(ctx, next){
+    let user = ctx.from.username
+    let table = 'bereitschaftslog'
+    let sql =`INSERT INTO ${table} (time, user, rang, status) VALUES("${get_date()}", "${user}", "EL", "aktiv");`
+    let result = db_query(ctx,sql)
+    if(result){
+        ctx.reply(`${user} meldet sich als EL QRV`)
+    };
+    logger.info(`${user} meldet sich als EL QRV`);
+}
+function elqrt(ctx, next){
+    let user = ctx.from.username
+    let table = 'bereitschaftslog'
+    let sql = `INSERT INTO ${table} (time, user, event) VALUES("${get_date()}", "${user}", "el inaktiv");`
+    let result = db_query(ctx,sql)
+    if(result){
+        ctx.reply(`${user} meldet sich als EL ab`)
+    };
+    logger.info(`${user} meldet sich als EL ab`);
+}
+function einsatzmeldung(ctx, next){
+    let user = ctx.from.username
+    let table = 'einsatzmeldungen'
+    let txt = ctx.message.text.substr(ctx.message.text.indexOf(" ") + 1);
+    let sql = `INSERT INTO ${table} (time, user, meldung) VALUES("${get_date()}", "${user}", "${txt}");`
+    let result = db_query(ctx,sql)
+    if(result){
+        ctx.reply("Meldung gelogt")
+    };
+    logger.info(`Meldung gelogt| ${user}: ${txt}`);
+}
+async function meldungsabfrage(ctx, next){
+    let user = ctx.from.username
+    let count = 5
+    let table = 'einsatzmeldungen'
+    DB.query(`SELECT * FROM ${table} ORDER BY id DESC LIMIT ${count};`, function (err, result, fields) {
+        if (err){
+            log(err);
+            logger.error(err);
+            return
+        }
+        let msg = " "
+        result.slice().reverse().forEach(x => {
+            msg =  msg + `${x.time} ${x.user} ${x.event}\n`
+        });
+        ctx.reply(msg)
+    });
+    logger.info(`meldungsabfrage generiert.`);
+}
+async function stabsabfrage(ctx, next){
+    let user = ctx.from.username
+    let count = 2
+    let table = "bereitschaftslog"
+    DB.query(`SELECT * FROM ${table} where status = "aktiv" ORDER BY id DESC LIMIT ${count};`, function (err, result, fields) {
+        if (err){
+            log(err);
+            logger.error(err);
+            return
+        }
+        let msg = " "
+        result.forEach(x => {
+            msg =  msg + `${x.user} = ${x.rang}\n`
+        });
+        ctx.reply(msg)
+    });
+    logger.info(`stabsabfrage generiert.`);
+}
+bot.launch()
